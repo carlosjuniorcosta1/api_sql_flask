c8347098eb140fd47b56eee037e64e43f09d38ef
José Costa <carlosjuniorcosta1@gmail.com>
2023-07-31 15:28:16 -0300

    endpoint Get por id adicionado

--- api_com_sql.py
+++ api_com_sql.py
@@ -16,19 +16,19 @@ dados_conexao = (
 conexao = pyodbc.connect(dados_conexao)
 cursor = conexao.cursor()
 
-cursor.commit()
-
-teste_q = cursor.execute('SELECT * FROM Vendas')
-teste_q = teste_q.fetchall()
-for x in teste_q:
-    for y in x:
-        print(y)
-
+bd = cursor.execute(f"SELECT * FROM Vendas WHERE id=4")
+dado_sel = bd.fetchone()
+lista_valores = []
+for x in dado_sel:
+    lista_valores.append(x)
+lista_chaves = ["id", "cliente", "produto"]
+resultado_id = dict(zip(lista_chaves, lista_valores))
+teste = [resultado_id]
 
 
 
 @app.route('/produtos', methods = ['GET'])
-def obter_livros():
+def listar_todos():
     dados_get = cursor.execute('SELECT * FROM Vendas')
     dados_get = dados_get.fetchall()
     data = []
@@ -40,6 +40,21 @@ def obter_livros():
         
         })
     return jsonify(message = "Esses são os dados solicitados", data = data)
+
+@app.route('/produtos/<id_linha>', methods = ['GET'])
+def listar_um(id_linha):
+    bd = cursor.execute(f"SELECT * FROM Vendas WHERE id=?", (id_linha))
+    dado_sel = bd.fetchone()
+    lista_valores = []
+    for x in dado_sel:
+        lista_valores.append(x)
+    lista_chaves = ["id", "cliente", "produto"]
+    resultado_id = dict(zip(lista_chaves, lista_valores))
+    return jsonify(message = "esse é o dado solicitado", data = resultado_id)
+
+
+
+
 
 @app.route('/produtos', methods = ['POST'])
 def adicionar_produto():
@@ -56,10 +71,9 @@ def adicionar_produto():
 
 @app.route('/produtos/<id_linha>', methods = ['DELETE'])
 def deletar_dado(id_linha):
-    db = cursor.execute(f"DELETE FROM Vendas WHERE id=?", (id_linha))
+    cursor.execute(f"DELETE FROM Vendas WHERE id=?", (id_linha))
     cursor.commit()
     return jsonify(message = "Item deletado")
-
 
 
 #roda o app  
